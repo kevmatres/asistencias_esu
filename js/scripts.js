ba26b2524acbcf4c9de005a8bfc2ec158119e742
kevmatres <82811078+kevmatres@users.noreply.github.com>
2025-01-07 06:38:41 -0500

    Delete js directory

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',function () {
-    const boton_abierto = document.getElementById('bot_crear_reunion');
-
-    boton_abierto.addEventListener('click', function () {
-        form_crear_reuinon.classList.remove('hidden');
-    });
-
-});
